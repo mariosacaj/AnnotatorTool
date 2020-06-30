11e9ca2a4cbef100866774528541ebb864cdbc90
Mario Sacaj <mario.sacaj@mail.polimi.it>
2020-06-30 19:16:12 +0200

    refinements, better readability

--- src/main/java/com/sprint/annotation/model/JavaCodeModel.java
+++ src/main/java/com/sprint/annotation/model/JavaCodeModel.java
@@ -24,7 +24,7 @@ public class JavaCodeModel {
     private Iterator<JDefinedClass> classes= null;
 
     /**
-     * Generates Java classes in targetPath directory given an XML schema.
+     * Generates JCodeModel and stores it for future annotation and final build/writedown to targetPath
      *
      * @param schemaFile file reference to the XML schema
      * @param targetPath directory where class source will be generated
@@ -49,20 +49,32 @@ public class JavaCodeModel {
         jcm = s2.generateCode(null, null);
     }
 
+    /**
+     * Writedown of the annotated JCodeModel to targetPath
+     *
+     * @throws IOException failure during build
+     */
     public void build() throws IOException {
         try (PrintStream status = new PrintStream(new ByteArrayOutputStream())) {
             jcm.build(targetPath, status);
         }
     }
 
-    public void setUri(String URI) {
+
+    /**
+     * Before actual annotation we need to identify the correct classes
+     * amongst all the classes produced by the schema compiler
+     *
+     * @param URI namespace of schemaFile
+     */
+    public void setURI(String URI) {
         this.loadClasses(NameConverter.standard.toPackageName(URI));
     }
 
     private void loadClasses(String package_name) {
         JPackage pck = jcm._package(package_name);
         Iterator<JDefinedClass> itr = pck.classes();
-        ArrayList<JDefinedClass> jDefinedClasses = new ArrayList<JDefinedClass>();
+        ArrayList<JDefinedClass> jDefinedClasses = new ArrayList<>();
 
         while (itr.hasNext()) {
             JDefinedClass t = itr.next();
@@ -70,6 +82,28 @@ public class JavaCodeModel {
                 jDefinedClasses.add(t);
         }
         classes = jDefinedClasses.iterator();
+    }
+
+
+    /**
+     * Insert confirmed mapping as a annotation into the JCodeModel tree
+     *
+     * @param standard_name element to map to concept (ex. 'fareTravelUrl')
+     * @param reference_name the reference concept in the target ontology (ex. 'st4rt:Travel')
+     * @param reference_type type of concept, must be 'C' if reference_name is a class
+     *                       in the ontology, 'P' if it is a property
+     * @throws ClassNotFoundException cannot find element to map
+     * @throws InputMismatchException trying to annotate a class as if it is a property or viceversa
+     */
+    public void writeDownAnnotation(String standard_name, String reference_name, char reference_type) throws ClassNotFoundException, InputMismatchException {
+        JAnnotatable annotatable = this.searchByName(standard_name);
+        if (annotatable instanceof JDefinedClass && reference_type == 'C')
+            annotatable.annotate(jcm.ref(RdfsClass.class)).param("value", reference_name);
+        else if (annotatable instanceof JFieldVar && reference_type == 'P')
+            annotatable.annotate(jcm.ref(RdfProperty.class)).param("propertyName", reference_name);
+        else
+            throw new InputMismatchException();
+
     }
 
     private JAnnotatable searchByName(String name) throws ClassNotFoundException {
@@ -141,23 +175,27 @@ public class JavaCodeModel {
         return null;
     }
 
-
-    public void writeDownAnnotation(String standard_name, String reference_name, char reference_type) throws ClassNotFoundException, InputMismatchException {
-        JAnnotatable annotatable = this.searchByName(standard_name);
-        if (annotatable instanceof JDefinedClass && reference_type == 'C')
-            annotatable.annotate(jcm.ref(RdfsClass.class)).param("value", reference_name);
-        else if (annotatable instanceof JFieldVar && reference_type == 'P')
-            annotatable.annotate(jcm.ref(RdfProperty.class)).param("propertyName", reference_name);
-        else
-            throw new InputMismatchException();
-
-    }
-
     public int test() {
         if (jcm != null) {
             return jcm.countArtifacts();
         }
         return -1;
+    }
+
+    private void add_fold(final String schemaPath) throws FileNotFoundException {
+        File schemaFile = new File(schemaPath);
+        final SchemaCompiler sc = XJC.createSchemaCompiler();
+        final FileInputStream schemaStream = new FileInputStream(schemaFile);
+        final InputSource is = new InputSource(schemaStream);
+        is.setSystemId(schemaFile.getAbsolutePath());
+
+        sc.parseSchema(is);
+        sc.setDefaultPackageName(null);
+
+        final S2JJAXBModel s2 = sc.bind();
+        JCodeModel jcm_ = s2.generateCode(null, null);
+        // @TODO in caso di selezione di più standard file
+
     }
 
 }
